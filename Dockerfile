d3185d800d9b8567ab634e245015314322d7c4cf
Kenura-R-Gunarathna <kenuragunarathna@gmail.com>
2024-03-31 01:41:12 +0530

    save chnages

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Use the official PHP image as base
+FROM php:8.1-apache
+
+# Install dependencies and PHP extensions
+RUN apt-get update && apt-get install -y \
+    libpng-dev \
+    libjpeg-dev \
+    libfreetype6-dev \
+    libzip-dev \
+    zip \
+    unzip \
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
+    && docker-php-ext-install -j$(nproc) gd pdo_mysql zip
+
+# Install Composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Set the working directory in the container
+WORKDIR /var/www/html
+
+# Copy the application code to the container
+COPY . /var/www/html
+
+# Expose port 80 (Apache)
+EXPOSE 80
+
+# Start Apache in the foreground
+CMD ["apache2-foreground"]
--- resources/views/livewire/pages/create-image.blade.php
+++ resources/views/livewire/pages/create-image.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use function Livewire\Volt\{usesFileUploads, layout, title, state, rules};
+use Illuminate\Support\Str;
 use App\Models\Image;
  
 usesFileUploads();
@@ -11,19 +12,20 @@ title('Create Images');
 
 state(['title' => '', 'description' => '', 'image']);
 
-rules(['title' => 'required|min:5|max:50', 'description' => 'required|min:5|max:500', 'image' => 'image|max:1024']);
+rules(['title' => 'required|min:5|max:50', 'description' => 'required|min:5|max:500', 'image' => 'required|image|max:1024']);
 
 $save = function () {
     
     $validated = $this->validate();
 
-    $this->image->store(path:'images', name:uuid()->toString());
-
+    $imageName = $this->image->storePubliclyAs(path: 'public/images', name: Str::uuid()->toString(). '.' . $this->image->getClientOriginalExtension());
+    $imagePath = str_replace('public/', 'storage/', $imageName);
+    
     $image = new Image;
     $image->title = $this->title;
     $image->description = $this->description;
-    $image->image_path = $this->image;
-    $image->store();
+    $image->image_path = $imagePath;
+    $image->save();
 
 };
  
@@ -41,7 +43,7 @@ $save = function () {
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
                 placeholder="Awsome foods" 
             />
-            @error('title') <span class="text-red-600">{{ $message }}</span> @enderror
+            @error('title') <span class="text-red-600 text-xs">{{ $message }}</span> @enderror
         </div>
         <div class="mb-5">
             <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Image description</label>
@@ -53,18 +55,22 @@ $save = function () {
                 placeholder="Write your image description here..."
             >
             </textarea>
-            @error('description') <span class="text-red-600">{{ $message }}</span> @enderror
+            @error('description') <span class="text-red-600 text-xs">{{ $message }}</span> @enderror
         </div>
         <div class="mb-5">
             <div class="flex items-center justify-center w-full">
-                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-fit border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50">
-                    <div class="flex flex-col items-center justify-center p-8">
-                        <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                        </svg>
-                        <p class="mb-2 text-sm text-gray-500"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                        <p class="text-xs text-gray-500">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                    </div>
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-fit border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 p-2">
+                    @if ($image) 
+                        <img class="rounded-lg" src="{{ $image->temporaryUrl() }}">
+                    @else
+                        <div class="flex flex-col items-center justify-center p-8">
+                            <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                            </svg>
+                            <p class="mb-2 text-sm text-gray-500"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p class="text-xs text-gray-500">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                        </div>
+                    @endif
                     <input
                         wire:model="image" 
                         id="dropzone-file" 
@@ -73,7 +79,7 @@ $save = function () {
                     />
                 </label>
             </div> 
-            @error('image') <span class="text-red-600">{{ $message }}</span> @enderror
+            @error('image') <span class="text-red-600 text-xs">{{ $message }}</span> @enderror
         </div>
         <button 
             type="submit" 
